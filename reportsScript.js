cdc25d928f733c9ec9cd60742d18ebbc8352c588
mjdean84 <43919438+mjdean84@users.noreply.github.com>
2019-10-20 23:57:23 -0400

    Add files via upload

--- reportsScript.js
+++ reportsScript.js
@@ -28,6 +28,10 @@ $(document).ready(function () {
         $('#clientsBody tr').filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
+
+        $('#salesBody tr').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
 
     //Type
@@ -254,6 +258,7 @@ function createInv() {
         new Product('Oven', 'Dacor', 'HWO230PS', '98561257', '30" Heritage Collection Pro Style Double Oven', 3985, 5399)
     );
     displayInv();
+    createSales();
 }
 
 function displayInv() {
@@ -297,3 +302,95 @@ function sortDes() {
 var clients = [];
 
 
+//Sales
+var salesArr = [];
+var datesArr = [];
+
+class Sale {
+    constructor(invoice, date, custID, emplID, desc, cost, price) {
+        this.invoice = invoice;
+        this.date = date;
+        this.custID = custID;
+        this.emplID = emplID;
+        this.desc = desc;
+        this.cost = cost;
+        this.price = price;
+    }
+}
+
+function createSales() {
+    salesArr.push(
+        new Sale(1, new Date('11/25/2018'), 120, 123456, 'Kitchen remodel', 7000, 12000),
+        new Sale(2, new Date('1/1/2019'), 658, 112244, 'Kitchen Retile', 3000, 5000),
+        new Sale(3, new Date('2/15/2019'), 23, 654987, 'New sinks kitchen and bath', 3500, 7500),
+        new Sale(4, new Date('4/7/2019'), 999, 998877, 'Bath remodel', 4500, 8000),
+        new Sale(5, new Date('4/28/2019'), 840, 888555, 'kitchen countertops', 3000, 5500),
+        new Sale(6, new Date('5/3/2019'), 1000, 111222, 'hardwood full house', 30000, 50000),
+        new Sale(7, new Date('5/7/2019'), 410, 444555, 'remodel full house', 100000, 1000000),
+        new Sale(8, new Date('5/9/2019'), 120, 123456, 'platinum/diamond microwave', 5000000, 100000000),
+        new Sale(9, new Date('5/10/2019'), 500, 456123, 'Bath remodel', 4500, 7500),
+        new Sale(10, new Date('7/12/2019'), 750, 654321, 'Kitchen and bath remodel', 22000, 30000)
+    );
+    displaySales(salesArr);
+}
+
+function findDates() {
+    datesArr.length = 0;
+    let date1 = $('#startDate').val();
+    let startDate = new Date(date1.replace(/-/g, '\/'));
+    let date2 = $('#endDate').val();
+    let endDate = new Date(date2.replace(/-/g, '\/'));
+
+    if (startDate == 'Invalid Date') {
+        $('#errorMessage').html('*Invalid start-date');
+    }
+    else if (endDate == 'Invalid Date') {
+        $('#errorMessage').html('*Invalid end-date');
+    }
+    else if (startDate > endDate) {
+        $('#errorMessage').html('*Start-date must be before end-date');
+    }
+    else {
+        $('#errorMessage').html('');
+    }
+
+    for (let i = 0; i < salesArr.length; i++) {
+        if (startDate <= salesArr[i].date && salesArr[i].date <= endDate) {
+            datesArr.push(salesArr[i]);
+        }
+    }
+    displaySales(datesArr);
+}
+
+function displaySales(arr) {
+    $('#salesBody').empty();
+    if (arr.length == 0) {
+        $('#errorMessage').html('*No records found');
+    }
+
+    let dateObj;
+    let month;
+    let day;
+    let year;
+    let date;
+
+    for (let i = 0; i < arr.length; i++) {
+        dateObj = arr[i].date;
+        month = dateObj.getUTCMonth();
+        month++;
+
+        day = dateObj.getUTCDate();
+        year = dateObj.getUTCFullYear();
+
+        date = month + "/" + day + "/" + year;
+
+        $('#salesBody').append('<tr><td>' + arr[i].invoice + '</td><td>' + date + '</td><td>' + arr[i].custID + '</td><td>' + arr[i].emplID + '</td><td class="text-capitalize">' + arr[i].desc + '</td><td>' + convUSD.format(arr[i].cost) + '</td><td>' + convUSD.format(arr[i].price) + '</td></tr>');
+    }
+}
+
+function resetDates() {
+    displaySales(salesArr);
+    $('#startDate').val('');
+    $('#endDate').val('');
+    $('#errorMessage').html('');
+}
